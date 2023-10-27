17deb1ca3ab650d82cd53b294a2a26b5442a34dc
Hypocisy <1137943609@qq.com>
2023-10-27 22:37:51 +0800

    custom tags

--- src/main/java/net/hypocisy/tutorialmod/item/custom/MetalDetectorItem.java
+++ src/main/java/net/hypocisy/tutorialmod/item/custom/MetalDetectorItem.java
@@ -1,17 +1,23 @@
 package net.hypocisy.tutorialmod.item.custom;
 
+import net.hypocisy.tutorialmod.util.ModTags;
 import net.minecraft.client.resources.language.I18n;
 import net.minecraft.core.BlockPos;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.item.context.UseOnContext;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockState;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
+import java.util.List;
 import java.util.Objects;
 
 public class MetalDetectorItem extends Item {
@@ -43,8 +49,15 @@ public class MetalDetectorItem extends Item {
                 player -> player.broadcastBreakEvent(player.getUsedItemHand()));
         return InteractionResult.SUCCESS;
     }
+
+    @Override
+    public void appendHoverText(ItemStack pStack, @Nullable Level pLevel, List<Component> pTooltipComponents, TooltipFlag pIsAdvanced) {
+        pTooltipComponents.add(Component.translatable("tooltip.tutorialmod.metal_detector.tooltip"));
+        super.appendHoverText(pStack, pLevel, pTooltipComponents, pIsAdvanced);
+    }
+
     private boolean isValuableBlock(BlockState state){
-        return state.is(Blocks.IRON_ORE) || state.is(Blocks.DIAMOND_ORE);
+        return state.is(ModTags.Blocks.METAL_DETECTOR_VALUABLES);
     }
     private void outputValuableCoordinates(BlockPos blockPos, Player player, Block block){
         player.sendSystemMessage(Component.literal("Found " + I18n.get(block.getDescriptionId()) + " at " +

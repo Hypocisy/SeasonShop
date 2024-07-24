de77db72df558278b42a65a70cc3ac5f4258526f
Hypocisy <1137943609@qq.com>
2024-07-24 23:28:21 +0800

    add package-info

---
+++ src/main/java/com/kumoe/SeasonShop/content/menu/package-info.java
@@ -0,0 +1,7 @@
+@MethodsReturnNonnullByDefault
+@ParametersAreNonnullByDefault
+package com.kumoe.SeasonShop.content.menu;
+
+import net.minecraft.MethodsReturnNonnullByDefault;
+
+import javax.annotation.ParametersAreNonnullByDefault;
